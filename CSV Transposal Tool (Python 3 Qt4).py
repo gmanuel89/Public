8762a77e7c178ec9b6fb4434e26adbf7dc5faff7
Manuel <gmanuel89+github@gmail.com>
2015-10-20 15:43:56 +0200

    Create CSV Transposal Tool (Python 3 Qt4).py

---
+++ CSV Transposal Tool (Python 3 Qt4).py
@@ -0,0 +1,73 @@
+################# CSV TRANSPOSAL
+
+## CSV LIKE THIS:
+# XXXXX
+# XXXX
+# XXXXXX
+# XXXX
+#
+# RESULT
+# XXXX
+# XXXX
+# XXXX
+# XXXX
+#  X X
+#  X
+
+import csv
+import sys
+from PyQt4.QtGui import *
+
+# Create a PyQt4 Application Object
+a = QApplication(sys.argv)
+
+# Message for selection (open)
+w = QWidget()
+selectionMessage = QMessageBox.information (w, "CSV selection", "Select the CSV file to be transposed")
+
+# CSV file selection
+w = QWidget()
+inputfile = QFileDialog.getOpenFileName (w, 'Open CSV', '', 'CSV files (*.csv)')
+print(inputfile)
+
+# Message for selection (save)
+w = QWidget()
+selectionMessage = QMessageBox.information (w, "CSV selection", "Select where to save the transposed CSV file")
+
+# Where to save the CSV file
+w = QWidget()
+outputfile = QFileDialog.getSaveFileName (w, 'Save CSV', '', 'CSV files (*.csv)')
+
+
+### Add the extension to the file automatically
+if ".csv" not in outputfile:
+    outputfile = str(outputfile) + ".csv"
+
+
+
+############################################# Transposal script
+# Open the input file (in a temporary variable f)
+with open(inputfile) as f:
+    # Read the actual csv values
+    csvFileRead = csv.reader(f)
+    # Create an empty array for storing the values (of the rows) to be stored in columns
+    csvColumns = []
+    # Scroll the rows of the CSV
+    for row in csvFileRead:
+        # Add the values in the row to the vector that will become the column
+        csvColumns.append(row)
+
+# Open the output file (in a temporary variable f)
+with open(outputfile, 'w') as f:
+    # Prepare to write the csv values with the function
+    csvFileWrite = csv.writer(f)
+    # max(csvColumns, key=len) iteratively scrolls the rows and returns the longest row, because it measures the length
+    # len (max (csvColumns, key=len)) returns the longest row
+    for i in range(len(max(csvColumns, key=len))):
+        # Write the values in columns in the output file
+        csvFileWrite.writerow([(c[i] if i<len(c) else '') for c in csvColumns])
+
+
+# Message for completion
+w = QWidget()
+selectionMessage = QMessageBox.information (w, "CSV transposed!!!", "The CSV file has been succesfully transposed")
