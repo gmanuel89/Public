56a1bde06b19d84bad51b73f3b83152e6961de3b
Manuel Galli <gmanuel89+github@gmail.com>
2017-05-19 17:29:49 +0200

    Update MASCOT OUTPUT MOD-PROCESSER (TclTk).R

--- MASCOT OUTPUT MOD-PROCESSER (TclTk).R
+++ MASCOT OUTPUT MOD-PROCESSER (TclTk).R
@@ -11,9 +11,9 @@ rm(list = ls())
 
 
 ### Program version (Specified by the program writer!!!!)
-R_script_version <- "2017.05.15.0"
+R_script_version <- "2017.05.19.0"
 ### GitHub URL where the R file is
-github_R_url <- "https://raw.githubusercontent.com/gmanuel89/Public-R-UNIMIB/master/MASCOT%20OUTPUT%20MOD-PROCESSER.R"
+github_R_url <- "https://raw.githubusercontent.com/gmanuel89/Mascot-Output-MOD-Processer/master/MASCOT%20OUTPUT%20MOD-PROCESSER.R"
 ### Name of the file when downloaded
 script_file_name <- "MASCOT OUTPUT MOD-PROCESSER"
 # Change log
